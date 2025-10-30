d9f19764103f9af7bf2ad9f6817e4880d952a28b
Pushkar Anand <pushkar8723@gmail.com>
2025-10-30 21:57:31 +0530

    Migrated Badge doc

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,34 +1,43 @@
-import { dirname, join } from "path";
-const path = require('path');
+import remarkGfm from 'remark-gfm';
 
-module.exports = {
-  stories: ['../stories/**/*.stories.hey.mdx', '../stories/**/*.stories.tsx'],
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
+  stories: [
+    '../stories/**/*.stories.tsx',
+    '../stories/**/*.stories.new.mdx',
+  ],
 
-  presets: [
+  addons: [
+    "@storybook/addon-webpack5-compiler-swc",
+    "@storybook/addon-links",
     {
-      name: '@storybook/preset-typescript',
+      name: '@storybook/addon-docs',
       options: {
-        include: [
-          path.resolve(__dirname, '../src'),
-          path.resolve(__dirname, '../stories'),
-        ],
+        mdxPluginOptions: {
+          mdxCompileOptions: {
+            remarkPlugins: [remarkGfm],
+          },
+        },
       },
     },
   ],
 
-  addons: [
-    getAbsolutePath("@storybook/addon-links"),
-    getAbsolutePath("@storybook/addon-docs"),
-    getAbsolutePath("@storybook/addon-mdx-gfm"),
-    getAbsolutePath("@storybook/addon-webpack5-compiler-swc")
-  ],
-
   framework: {
-    name: getAbsolutePath("@storybook/react-webpack5"),
+    name: "@storybook/react-webpack5",
     options: {}
-  }
+  },
+
+  docs: {
+    autodocs: true
+  },
+  
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+    },
+  },
 };
 
-function getAbsolutePath(value) {
-  return dirname(require.resolve(join(value, "package.json")));
-}
+export default config;
