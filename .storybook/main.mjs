4d3a0f52f65a7fd05b194a014285e21aa62d01b8
Pushkar Anand <pushkar8723@gmail.com>
2025-12-04 23:31:43 +0530

    Updated CI branch name (#22)
    * Updated CI branch name
    
    * Removed node 18 as it is not compatible with storybook
    
    * Fix CI check
    
    * Addressed copilot comments

--- .storybook/main.mjs
+++ .storybook/main.mjs
@@ -1,45 +1,42 @@
-import { fileURLToPath } from "node:url";
-import { dirname } from "node:path";
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
 import remarkGfm from 'remark-gfm';
 
 /** @type { import('@storybook/react-webpack5').StorybookConfig } */
 const config = {
-  stories: [
-    '../stories/**/*.stories.tsx',
-    '../stories/**/*.mdx',
-  ],
+    stories: ['../stories/**/*.stories.tsx', '../stories/**/*.mdx'],
 
-  addons: [
-    getAbsolutePath("@storybook/addon-webpack5-compiler-swc"),
-    getAbsolutePath("@storybook/addon-links"),
-    {
-      name: getAbsolutePath("@storybook/addon-docs"),
-      options: {
-        mdxPluginOptions: {
-          mdxCompileOptions: {
-            remarkPlugins: [remarkGfm],
-          },
+    addons: [
+        getAbsolutePath('@storybook/addon-webpack5-compiler-swc'),
+        getAbsolutePath('@storybook/addon-links'),
+        {
+            name: getAbsolutePath('@storybook/addon-docs'),
+            options: {
+                mdxPluginOptions: {
+                    mdxCompileOptions: {
+                        remarkPlugins: [remarkGfm],
+                    },
+                },
+            },
         },
-      },
-    },
-  ],
+    ],
 
-  framework: {
-    name: getAbsolutePath("@storybook/react-webpack5"),
-    options: {}
-  },
-
-  typescript: {
-    reactDocgen: 'react-docgen-typescript',
-    reactDocgenTypescriptOptions: {
-      shouldExtractLiteralValuesFromEnum: true,
-      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+    framework: {
+        name: getAbsolutePath('@storybook/react-webpack5'),
+        options: {},
     },
-  }
+
+    typescript: {
+        reactDocgen: 'react-docgen-typescript',
+        reactDocgenTypescriptOptions: {
+            shouldExtractLiteralValuesFromEnum: true,
+            propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+        },
+    },
 };
 
 export default config;
 
 function getAbsolutePath(value) {
-  return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)));
+    return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)));
 }
