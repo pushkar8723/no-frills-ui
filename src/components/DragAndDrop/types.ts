0918d54723cb53ec34178038cbdf48f16b1ba5d3
Pushkar Anand <pushkar8723@gmail.com>
2025-12-07 02:30:55 +0530

    Fix a11y issues in Drag and Drop component

--- src/components/DragAndDrop/types.ts
+++ src/components/DragAndDrop/types.ts
@@ -9,7 +9,29 @@ export const DragContext = createContext<{
     startIndex: number;
     setStartIndex: (value: number) => void;
     drop: (index: number) => void;
+    onDrop: (start: number, end: number) => void;
+    cancel: () => void;
+    startGrab: (index: number) => void;
     isDragging: boolean;
     setIsDragging: (value: boolean) => void;
     setDragOver: (value: number) => void;
+    i18n: {
+        itemAriaLabelTemplate: string;
+        dragHandleAriaLabel: string;
+        grabbedAnnouncementTemplate: string;
+        movedAnnouncementTemplate: string;
+        droppedAnnouncementTemplate: string;
+        cancelledAnnouncementTemplate: string;
+        replacePlaceholders: (
+            template: string,
+            data: {
+                position?: number;
+                grabKey?: string;
+                dropKey?: string;
+                altDropKey?: string;
+                cancelKey?: string;
+                moveKeys?: string;
+            },
+        ) => string;
+    };
 }>(null);
