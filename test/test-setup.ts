67b0b99c336543f6a001cf961c50088c28705369
Pushkar Anand <pushkar8723@gmail.com>
2025-12-21 01:09:55 +0530

    Added test for layer manager

--- test/test-setup.ts
+++ test/test-setup.ts
@@ -1,2 +1,11 @@
 import '@testing-library/jest-dom';
 import 'jest-axe/extend-expect';
+
+// Mock window.scrollTo since jsdom doesn't implement it
+Object.defineProperty(window, 'scrollTo', {
+    value: jest.fn(),
+    writable: true,
+});
+
+// Make window.setTimeout use the mocked global.setTimeout
+window.setTimeout = global.setTimeout;
