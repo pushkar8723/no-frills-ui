46b222843ccae65aa1ce7ef08d925c84bfad40a1
Pushkar Anand <pushkar8723@gmail.com>
2025-12-20 12:57:10 +0530

    Basic test setup

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+    roots: ['<rootDir>/test'],
+    testMatch: ['**/__tests__/**/*.test.(ts|tsx)', '**/?(*.)+(spec|test).(ts|tsx)'],
+    setupFilesAfterEnv: ['<rootDir>/test/test-setup.ts'],
+    collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.d.ts'],
+    coverageDirectory: 'coverage',
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1',
+    },
+    transformIgnorePatterns: ['node_modules/(?!(your-library-if-needed)/)'],
+};
