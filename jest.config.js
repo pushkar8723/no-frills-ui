7871cb74aa95440ba13b4298e40009150a1774d2
Pushkar Anand <pushkar8723@gmail.com>
2025-12-28 11:54:56 +0530

    Fix jest coverage report

--- jest.config.js
+++ jest.config.js
@@ -5,11 +5,22 @@ module.exports = {
     testMatch: ['**/__tests__/**/*.test.(ts|tsx)', '**/?(*.)+(spec|test).(ts|tsx)'],
     setupFilesAfterEnv: ['<rootDir>/test/test-setup.ts'],
     collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.d.ts'],
-    coverageProvider: 'v8',
+    coverageProvider: 'babel',
     coverageDirectory: 'coverage',
     coverageReporters: ['text', 'lcov'],
     moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/src/$1',
+    },
+    transform: {
+        '^.+\\.tsx?$': [
+            'ts-jest',
+            {
+                tsconfig: {
+                    sourceMap: true,
+                    inlineSources: true,
+                },
+            },
+        ],
     },
     transformIgnorePatterns: ['node_modules/(?!(your-library-if-needed)/)'],
 };
