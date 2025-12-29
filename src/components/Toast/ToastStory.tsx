46c40a5a8ea9328b855b828059c1319e73ed8fae
Pushkar Anand <pushkar8723@gmail.com>
2025-12-29 20:05:00 +0530

    Fixed Toast doc

--- src/components/Toast/ToastStory.tsx
+++ src/components/Toast/ToastStory.tsx
@@ -1,25 +1,4 @@
-import { TOAST_TYPE } from './Toast';
-
-type ToastOptions = {
-    /** Text of the toast */
-    text: string;
-    /** Id for the notification, helps in de-duplication. */
-    id?: string;
-    /**
-     * Duration for the toast in milliseconds
-     * @default 2000
-     */
-    duration?: number;
-    /**
-     * Type of toast
-     * @default TOAST_TYPE.NORMAL
-     */
-    type?: TOAST_TYPE;
-    /** Action button text */
-    buttonText?: string;
-    /** Action button click callback */
-    buttonClick?: () => void;
-};
+import { type ToastOptions } from './Toast';
 
 /** This component is only used for storybook documentation */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
